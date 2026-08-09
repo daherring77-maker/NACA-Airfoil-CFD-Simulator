d11d9f77a1f506597dea781823b4415e59b26960
daherring77-maker <daherring77@gmail.com>
2026-08-09 21:14:05 +0100

    Add files via upload

--- su2_airfoil2d.py
+++ su2_airfoil2d.py
@@ -8,6 +8,7 @@ pages = {
            st.Page("pages/su2_faqs.py", title="SU2 Code FAQs", icon="📊"),
            st.Page("pages/aero2d_runner.py", title="NACA CFD Streamlit", icon="📊"),
            st.Page("pages/cl_vs_aoa.py", title="CL versus AOA", icon="📊"),
+           st.Page("pages/cl_vs_flap_deflection.py", title="CL versus Flap Deflection", icon="📊"),
         ],
         
         }
